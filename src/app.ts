f69972a9bd96e852a649a15812ad3fd709a2077e
junhaohe-chris <junhao.he@thoughtworks.com>
2026-09-03 11:23:22 +0800

    Add cors middleware for cross-origin requests

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { type Express, type Request, type Response } from 'express';
 import { createTaskController, deleteTaskController, getTasksController, updateTaskController } from './controllers/task.controller.ts';
+import cors from 'cors';
 
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/health', (req: Request, res: Response) => {
